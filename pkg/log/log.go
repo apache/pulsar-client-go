3da0181535088668c25d55d9c174898557722c7e
xiaolong.ran <ranxiaolong716@gmail.com>
2019-05-16 15:37:20 +0800

    fix a little
    Signed-off-by: xiaolong.ran <ranxiaolong716@gmail.com>

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -126,7 +126,7 @@ func (h *hook) Levels() []log.Level {
 // isSKippedPackageName tests wether path name is on log library calling stack.
 func isSkippedPackageName(name string) bool {
     return strings.Contains(name, "github.com/sirupsen/logrus") ||
-            strings.Contains(name, "github.com/apache/pulsar/pulsar-client-go/logutil")
+            strings.Contains(name, "github.com/apache/pulsar-client-go/pkg/log")
 }
 
 // formatter is for compatibility with ngaut/log
