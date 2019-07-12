2c536880f0f12b6ecc197d1b8a9933b9bdc10ab1
Sijie Guo <sijie@apache.org>
2019-07-11 22:26:55 -0700

    Initialize the pulsarctl project (streamnative/pulsarctl#1)
    * Initialize the pulsarctl project
    
    *Modifications*
    
    - setup the project layout
    - add two clusters command

---
+++ pulsaradmin/pkg/pulsar/cluster.go
@@ -0,0 +1,48 @@
+package pulsar
+
+// Clusters  is used to access the cluster endpoints.
+
+type Clusters interface {
+	List() ([]string, error)
+	Get(string) (ClusterData, error)
+	Create(ClusterData) error
+	Delete(string) error
+}
+
+type clusters struct {
+	client 		*client
+	basePath 	string
+}
+
+func (c *client) Clusters() Clusters {
+	return &clusters{
+		client: c,
+		basePath:"/clusters",
+	}
+}
+
+func (c *clusters) List() ([]string, error) {
+	var clusters []string
+	err := c.client.get(c.client.endpoint(c.basePath), &clusters)
+	return clusters, err
+}
+
+func (c *clusters) Get(name string) (ClusterData, error) {
+	cdata := ClusterData{}
+	endpoint := c.client.endpoint(c.basePath, name)
+	err := c.client.get(endpoint, &cdata)
+	return cdata, err
+}
+
+func (c *clusters) Create(cdata ClusterData) error {
+	endpoint := c.client.endpoint(c.basePath, cdata.Name)
+	err := c.client.put(endpoint, &cdata, nil)
+	return err
+}
+
+func (c *clusters) Delete(name string) error {
+	endpoint := c.client.endpoint(c.basePath, name)
+	return c.client.delete(endpoint, nil)
+}
+
+
---
+++ pulsaradmin/pkg/pulsar/utils.go
@@ -0,0 +1,7 @@
+package pulsar
+
+import "fmt"
+
+func makeHttpPath(apiVersion string, componentPath string) string {
+	return fmt.Sprintf("/admin/%s%s", apiVersion, componentPath)
+}
