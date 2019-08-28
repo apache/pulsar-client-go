64b9813c0d91d2aeca381461e1ea254491fba84a
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-08-28 16:18:25 +0800

    Add cluster commands `get` (streamnative/pulsarctl#4)
    Add cluster commands `get`

--- pulsaradmin/go.mod
+++ pulsaradmin/go.mod
@@ -4,11 +4,15 @@ go 1.12
 
 require (
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/kris-nova/logger v0.0.0-20181127235838-fd0d87064b06
 	github.com/kris-nova/lolgopher v0.0.0-20180921204813-313b3abb0d9b // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-runewidth v0.0.4 // indirect
 	github.com/olekukonko/tablewriter v0.0.1
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.3
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.3.0
 )
---
+++ pulsaradmin/pkg/pulsar/descriptions_test.go
@@ -0,0 +1,52 @@
+package pulsar
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLongDescription_exampleToString(t *testing.T) {
+	desc := LongDescription{}
+	example := Example{
+		Desc:    "command description",
+		Command: "command",
+	}
+	desc.CommandExamples = []Example{example}
+	res := desc.exampleToString()
+
+	expect := "    #command description\n" +
+		"    command\n"
+
+	assert.Equal(t, expect, res)
+}
+
+func TestLongDescription_ToString(t *testing.T) {
+	desc := LongDescription{}
+	desc.CommandUsedFor = "command used for"
+	desc.CommandPermission = "command permission"
+	example := Example{}
+	example.Desc = "command description"
+	example.Command = "command"
+	desc.CommandExamples = []Example{example}
+	out := Output{
+		Desc: "Output",
+		Out:  "Out line 1\nOut line 2",
+	}
+	desc.CommandOutput = []Output{out}
+
+	expect := "USED FOR:\n" +
+		"    " + desc.CommandUsedFor + "\n\n" +
+		"REQUIRED PERMISSION:\n" +
+		"    " + desc.CommandPermission + "\n\n" +
+		"EXAMPLES:\n" +
+		"    " + "#" + example.Desc + "\n" +
+		"    " + example.Command + "\n\n" +
+		"OUTPUT:\n" +
+		"    " + "#" + out.Desc + "\n" +
+		"    " + "Out line 1" + "\n" +
+		"    " + "Out line 2" + "\n\n"
+
+	result := desc.ToString()
+
+	assert.Equal(t, expect, result)
+}
