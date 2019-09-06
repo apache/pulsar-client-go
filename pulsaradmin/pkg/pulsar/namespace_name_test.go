353031990031d56b51d68367476dbe82d62847d0
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-06 10:40:56 +0800

    Add namespace and topic name check (streamnative/pulsarctl#37)
    Add namespace and topic name check

---
+++ pulsaradmin/pkg/pulsar/namespace_name_test.go
@@ -0,0 +1,46 @@
+package	pulsar
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetNamespaceName(t *testing.T) {
+	success, err := GetNamespaceName("public/default")
+	assert.Nil(t, err)
+	assert.Equal(t, "public/default", success.String())
+
+	empty, err := GetNamespaceName("")
+	assert.NotNil(t, err)
+	assert.Equal(t, "The namespace complete name is empty.", err.Error())
+	assert.Nil(t, empty)
+
+	empty, err = GetNamespaceName("/")
+	assert.NotNil(t, err)
+	assert.Equal(t, "Invalid tenant or namespace. [/]", err.Error())
+	assert.Nil(t, empty)
+
+	invalid, err := GetNamespaceName("public/default/fail")
+	assert.NotNil(t, err)
+	assert.Equal(t, "The complete name of namespace is invalid. complete name : [public/default/fail]", err.Error())
+	assert.Nil(t, invalid)
+
+	invalid, err = GetNamespaceName("public")
+	assert.NotNil(t, err)
+	assert.Equal(t, "The complete name of namespace is invalid. complete name : [public]", err.Error())
+	assert.Nil(t, invalid)
+
+	special, err := GetNamespaceName("-=.:/-=.:")
+	assert.Nil(t, err)
+	assert.Equal(t, "-=.:/-=.:", special.String())
+
+	tenantInvalid, err := GetNamespaceName("\"/namespace")
+	assert.NotNil(t, err)
+	assert.Equal(t, "Tenant name include unsupported special chars. tenant : [\"]", err.Error())
+	assert.Nil(t, tenantInvalid)
+
+	namespaceInvalid, err := GetNamespaceName("tenant/}")
+	assert.NotNil(t, err)
+	assert.Equal(t, "Namespace name include unsupported special chars. namespace : [}]", err.Error())
+	assert.Nil(t, namespaceInvalid)
+}
