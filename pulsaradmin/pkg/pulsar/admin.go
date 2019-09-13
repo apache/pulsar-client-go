5db1e694c7f4d95023bdeeb77daf7aa794df96bc
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-09-14 04:19:35 +0800

    Add command topic `lookup` and `bundle-range` (streamnative/pulsarctl#44)
    ## OUTPUT
    
    *lookup*
    ```
    ➜ pulsarctl-yong git:(topic-lookup) ✗ ./pulsarctl topic lookup -h
    USED FOR:
        This command is used for looking up a topic.
    
    REQUIRED PERMISSION:
        This command does not require permissions.
    
    EXAMPLES:
        #Look up a topic <topic-name>
        pulsarctl topic lookup <topic-name>
    
    OUTPUT:
        #
        {
          "brokerUlr": "",
          "brokerUrlTls": "",
          "httpUrl": "",
          "httpUrlTls": "",
        }
    
        #the topic name is not specified
        [✖]  only one argument is allowed to be used as a name
    
        #the topic name is not in the format of <tenant>/<namespace>/<topic> or <topic>
        [✖]  Invalid short topic name '<topic-name>', it should be in the format of <tenant>/<namespace>/<topic> or <topic>
    
        #the topic name is not in the format of <domain>://<tenant>/<namespace>/<topic>
        [✖]  Invalid complete topic name '<topic-name>', it should be in the format of <domain>://<tenant>/<namespace>/<topic>
    
        #the topic name is not in the format of <tenant>/<namespace>/<topic>
        [✖]  Invalid topic name '<topic-name>', it should be in the format of<tenant>/<namespace>/<topic>
    
        #the namespace name is not in the format of <tenant>/<namespace>
        [✖]  The complete name of namespace is invalid. complete name : <namespace-complete-name>
    
        #the tenant name and(or) namespace name is empty
        [✖]  Invalid tenant or namespace. [<tenant>/<namespace>]
    
        #the tenant name contains unsupported special chars. the alphanumeric (a-zA-Z0-9) and the special chars (-=:.%)  is allowed
        [✖]  Tenant name include unsupported special chars. tenant : [<namespace>]
    
        #the namespace name contains unsupported special chars. the  alphanumeric (a-zA-Z0-9) and the special chars (-=:.%) is allowed
        [✖]  Namespace name include unsupported special chars. namespace : [<namespace>]
    
    Usage: pulsarctl topics lookup [flags]
    ```
    
    *bundle-range*
    ```
    ➜ pulsarctl-yong git:(topic-lookup) ✗ ./pulsarctl topic bundle-range -h
    USED FOR:
        This command is used for getting namespace bundle range of a topic.
    
    REQUIRED PERMISSION:
        This command requires super-user permissions.
    
    EXAMPLES:
        #Get namespace bundle range of a topic <topic-name>
        pulsarctl topic bundle-range <topic-name>
    
    OUTPUT:
        #normal output
        The bundle range of the topic <topic-name> is: <bundle-range>
    
        #the topic name is not specified
        [✖]  only one argument is allowed to be used as a name
    
        #the topic name is not in the format of <tenant>/<namespace>/<topic> or <topic>
        [✖]  Invalid short topic name '<topic-name>', it should be in the format of <tenant>/<namespace>/<topic> or <topic>
    
        #the topic name is not in the format of <domain>://<tenant>/<namespace>/<topic>
        [✖]  Invalid complete topic name '<topic-name>', it should be in the format of <domain>://<tenant>/<namespace>/<topic>
    
        #the topic name is not in the format of <tenant>/<namespace>/<topic>
        [✖]  Invalid topic name '<topic-name>', it should be in the format of<tenant>/<namespace>/<topic>
    
        #the namespace name is not in the format of <tenant>/<namespace>
        [✖]  The complete name of namespace is invalid. complete name : <namespace-complete-name>
    
        #the tenant name and(or) namespace name is empty
        [✖]  Invalid tenant or namespace. [<tenant>/<namespace>]
    
        #the tenant name contains unsupported special chars. the alphanumeric (a-zA-Z0-9) and the special chars (-=:.%)  is allowed
        [✖]  Tenant name include unsupported special chars. tenant : [<namespace>]
    
        #the namespace name contains unsupported special chars. the  alphanumeric (a-zA-Z0-9) and the special chars (-=:.%) is allowed
        [✖]  Namespace name include unsupported special chars. namespace : [<namespace>]
    
    Usage: pulsarctl topics bundle-range [flags]
    
    Aliases: bundle-range,
    ```

--- pulsaradmin/pkg/pulsar/admin.go
+++ pulsaradmin/pkg/pulsar/admin.go
@@ -143,25 +143,36 @@ func (c *client) endpoint(componentPath string, parts ...string) string {
 
 // get is used to do a GET request against an endpoint
 // and deserialize the response into an interface
-func (c *client) get(endpoint string, obj interface{}) error {
+func (c *client) getAndDecode(endpoint string, obj interface{}, decode bool) ([]byte, error) {
 	req, err := c.newRequest(http.MethodGet, endpoint)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	resp, err := checkSuccessful(c.doRequest(req))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer safeRespClose(resp)
 
 	if obj != nil {
 		if err := decodeJsonBody(resp, &obj); err != nil {
-			return err
+			return nil, err
 		}
+	} else if !decode {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return body, err
 	}
 
-	return nil
+	return nil, err
+}
+
+func (c *client) get(endpoint string, obj interface{}) error {
+	_, err := c.getAndDecode(endpoint, obj, true)
+	return err
 }
 
 func (c *client) put(endpoint string, in, obj interface{}) error {
