204626b38e6f25047497d518ba2e49bcf9a64d9b
Jim Lambert <jim_lambert@bose.com>
2019-12-09 08:04:27 -0500

    allow go module to be installed with go get, by adding a stub source to the root dir

---
+++ no_code.go
@@ -0,0 +1,29 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// There is only one purpose for this file: to allow the go module
+// to be installed.  At the moment, without no_code.go there is only
+// one source file in the root directory of the module and it's a
+// *_test.go file... this keeps the module from being installed via
+// go get.
+
+
+
+package main
+
+func main() {
+}
