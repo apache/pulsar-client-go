50b7aa5a08c0ad41642e0547953cf1d5d43b7763
xiaolong.ran <rxl@apache.org>
2019-12-23 19:57:06 +0800

    fix ci error
    Signed-off-by: xiaolong.ran <rxl@apache.org>

--- docker-ci.sh
+++ docker-ci.sh
@@ -21,7 +21,7 @@
 set -e -x
 
 # Project code style check
-docker run --rm -v $(pwd):/app -w /app golangci/golangci-lint:v1.21.0 golangci-lint run -c ./.golangci.yml ./...
+docker run --rm -v $(pwd):/app -w /app golangci/golangci-lint:v1.21.0 golangci-lint run -v -c ./.golangci.yml ./...
 
 SRC_DIR=$(git rev-parse --show-toplevel)
 cd ${SRC_DIR}
