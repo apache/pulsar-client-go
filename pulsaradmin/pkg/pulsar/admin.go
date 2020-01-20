46f9c69bf4f271a325f392c2d2347bcb204bbce0
冉小龙 <rxl@apache.org>
2020-01-20 17:16:29 +0800

    [issue:182] get-function-assignments has incorrect output (streamnative/pulsarctl#199)
    Signed-off-by: xiaolong.ran <rxl@apache.org>
    
    get-function-assignments has incorrect output

--- pulsaradmin/pkg/pulsar/admin.go
+++ pulsaradmin/pkg/pulsar/admin.go
@@ -76,7 +76,6 @@ func New(config *common.Config) (Client, error) {
 
 	authProvider, err := auth.GetAuthProvider(config)
 	if !utils.IsNilFixed(authProvider) {
-		fmt.Printf("Found Auth provider %T\n", authProvider)
 		c.Client.HTTPClient.Transport = *authProvider
 	} else {
 		fmt.Printf("No Auth Provider found\n")
