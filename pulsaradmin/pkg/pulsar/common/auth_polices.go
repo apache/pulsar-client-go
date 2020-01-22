96d284a78f253965855b31fe50f8847a84f7c7ad
Nathan Mills <nathanm@instructure.com>
2020-01-22 02:30:52 -0700

    Change NameSpaceAuth to be an [] of AuthAction (streamnative/pulsarctl#202)
    * Change NameSpaceAuth to be an [] of AuthAction
    
    * Format auth_polices

--- pulsaradmin/pkg/pulsar/common/auth_polices.go
+++ pulsaradmin/pkg/pulsar/common/auth_polices.go
@@ -18,14 +18,14 @@
 package common
 
 type AuthPolicies struct {
-	NamespaceAuth         map[string]AuthAction            `json:"namespace_auth"`
+	NamespaceAuth         map[string][]AuthAction          `json:"namespace_auth"`
 	DestinationAuth       map[string]map[string]AuthAction `json:"destination_auth"`
 	SubscriptionAuthRoles map[string][]string              `json:"subscription_auth_roles"`
 }
 
 func NewAuthPolicies() *AuthPolicies {
 	return &AuthPolicies{
-		NamespaceAuth:         make(map[string]AuthAction),
+		NamespaceAuth:         make(map[string][]AuthAction),
 		DestinationAuth:       make(map[string]map[string]AuthAction),
 		SubscriptionAuthRoles: make(map[string][]string),
 	}
