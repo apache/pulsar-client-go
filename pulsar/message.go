4729eca09fa96863c3713b3c70e801d1d2479ca7
xiaolong.ran <rxl@apache.org>
2020-06-26 12:21:57 +0800

    fix comments
    Signed-off-by: xiaolong.ran <rxl@apache.org>

--- pulsar/message.go
+++ pulsar/message.go
@@ -64,7 +64,7 @@ type Message interface {
 	// Topic get the topic from which this message originated from
 	Topic() string
 
-	// ProducerName will get producer name from broker
+	// ProducerName will get the producer name who produced this message.
 	ProducerName() string
 
 	// Properties are application defined key/value pairs that will be attached to the message.
