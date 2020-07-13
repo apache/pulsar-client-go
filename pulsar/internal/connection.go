e51daf37d487516d091e3c710e164712e218d928
fish <gcslyp@gmail.com>
2020-07-13 23:20:29 +0800

    optimization log when connection failed

--- pulsar/internal/connection.go
+++ pulsar/internal/connection.go
@@ -287,7 +287,7 @@ func (c *connection) doHandshake() bool {
 	c.cnx.SetDeadline(time.Time{})
 
 	if cmd.Connected == nil {
-		c.log.Warnf("Failed to perform initial handshake - Expecting 'Connected' cmd, got '%s'",
+		c.log.Warnf("Failed to establish connection with broker: '%s'",
 			cmd.Error.GetMessage())
 		return false
 	}
