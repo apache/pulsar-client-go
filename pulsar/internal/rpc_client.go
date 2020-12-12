apache/pulsar-client-go

commit 86c3e808d0e6521757a54a6908e24d42fc099734

Add retry logic when get connection error (#157)
* [issue:144] Add retry logic when get connection error

Signed-off-by: xiaolong.ran <rxl@apache.org>

* replace fmt with log

Signed-off-by: xiaolong.ran <rxl@apache.org>

* code formate

Signed-off-by: xiaolong.ran <rxl@apache.org>

* add coverage.html to .gitignore

Signed-off-by: xiaolong.ran <rxl@apache.org>

* fix comments

Signed-off-by: xiaolong.ran <rxl@apache.org>

* code format

Signed-off-by: xiaolong.ran <rxl@apache.org>

pulsar/internal/rpc_client.go
@@ -26,6 +26,8 @@ import (
 
 	"github.com/apache/pulsar-client-go/pulsar/internal/pb"
 	"github.com/golang/protobuf/proto"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type RPCResult struct {
@@ -59,6 +61,8 @@ type rpcClient struct {
 	requestIDGenerator  uint64
 	producerIDGenerator uint64
 	consumerIDGenerator uint64
+
+	log *log.Entry
 }
 
 func NewRPCClient(serviceURL *url.URL, pool ConnectionPool, requestTimeout time.Duration) RPCClient {
@@ -66,6 +70,7 @@ func NewRPCClient(serviceURL *url.URL, pool ConnectionPool, requestTimeout time.
 		serviceURL:     serviceURL,
 		pool:           pool,
 		requestTimeout: requestTimeout,
+		log:            log.WithField("serviceURL", serviceURL),
 	}
 }
 
@@ -76,8 +81,7 @@ func (c *rpcClient) RequestToAnyBroker(requestID uint64, cmdType pb.BaseCommand_
 
 func (c *rpcClient) Request(logicalAddr *url.URL, physicalAddr *url.URL, requestID uint64,
 	cmdType pb.BaseCommand_Type, message proto.Message) (*RPCResult, error) {
-	// TODO: Add retry logic in case of connection issues
-	cnx, err := c.pool.GetConnection(logicalAddr, physicalAddr)
+	cnx, err := c.getConn(logicalAddr, physicalAddr)
 	if err != nil {
 		return nil, err
 	}
@@ -88,7 +92,7 @@ func (c *rpcClient) Request(logicalAddr *url.URL, physicalAddr *url.URL, request
 	}
 	ch := make(chan Res)
 
-	// TODO: Handle errors with disconnections
+	// TODO: in here, the error of callback always nil
 	cnx.SendRequest(requestID, baseCommand(cmdType, message), func(response *pb.BaseCommand, err error) {
 		ch <- Res{&RPCResult{
 			Cnx:      cnx,
@@ -103,6 +107,26 @@ func (c *rpcClient) Request(logicalAddr *url.URL, physicalAddr *url.URL, request
 	case <-time.After(c.requestTimeout):
 		return nil, errors.New("request timed out")
 	}
+}
+
+func (c *rpcClient) getConn(logicalAddr *url.URL, physicalAddr *url.URL) (Connection, error) {
+	cnx, err := c.pool.GetConnection(logicalAddr, physicalAddr)
+	backoff := new(Backoff)
+	var retryTime time.Duration
+	if err != nil {
+		for retryTime < c.requestTimeout {
+			retryTime = backoff.Next()
+			c.log.Debugf("Reconnecting to broker in {%v}", retryTime)
+			time.Sleep(retryTime)
+			cnx, err = c.pool.GetConnection(logicalAddr, physicalAddr)
+			if err == nil {
+				c.log.Debugf("retry connection success")
+				return cnx, nil
+			}
+		}
+		return nil, err
+	}
+	return cnx, nil
 }
 
 func (c *rpcClient) RequestOnCnx(cnx Connection, requestID uint64, cmdType pb.BaseCommand_Type,

.gitignore
@@ -9,6 +9,7 @@
 
 # Output of the go coverage tool
 *.out
+coverage.html
 
 perf/perf
 pulsar-perf

pulsar/internal/connection_reader.go
@@ -50,7 +50,7 @@ func (r *connectionReader) readFromConnection() {
 		}
 
 		// Process
-		var payloadLen uint32 = 0
+		var payloadLen uint32
 		if headersAndPayload != nil {
 			payloadLen = headersAndPayload.ReadableBytes()
 		}

commit 0296890a9136a0921da19442cf713a4ebb31f9b0

Fixed logic to attempt reconnections to same broker (#414)
### Motivation

There is a problem with the re-connection logic introduced in #157. 

The change added a logic to keep retrying to establish a TCP connection with broker up to the "operation timeout" (default 30seconds). 

There are few issues with it: 
 1. (minor) It's not checking that the error is indeed a TCP error (eg: it would retry on auth failures too)
 2. (major) After a TCP connection failure, reconnecting to the same broker is always the wrong approach, because the most likely outcome is that the next attempt will also fail and, worse, the IP might just be unresponsive and we will then have to wait for the full connection timeout time. 

The correct solution after a connection failure is to re-do the topic lookup, since the topic will be moving to a different broker and we need to reconnect to the new broker asap. 

The only time we can do this connection retry logic is for requests that are not specific to a particular broker (eg: lookup operations). In this case a quick retry on a connection failure will probably land the request on a different, healthy, broker.

pulsar/internal/rpc_client.go
@@ -19,6 +19,7 @@ package internal
 
 import (
 	"errors"
+	"net"
 	"net/url"
 	"sync"
 	"sync/atomic"
@@ -78,13 +79,37 @@ func NewRPCClient(serviceURL *url.URL, pool ConnectionPool,
 
 func (c *rpcClient) RequestToAnyBroker(requestID uint64, cmdType pb.BaseCommand_Type,
 	message proto.Message) (*RPCResult, error) {
-	return c.Request(c.serviceURL, c.serviceURL, requestID, cmdType, message)
+
+	rpcResult, err := c.Request(c.serviceURL, c.serviceURL, requestID, cmdType, message)
+	if _, ok := err.(net.Error); ok {
+		// We can retry this kind of requests over a connection error because they're
+		// not specific to a particular broker.
+		backoff := Backoff{100 * time.Millisecond}
+		startTime := time.Now()
+		var retryTime time.Duration
+
+		for time.Since(startTime) < c.requestTimeout {
+			retryTime = backoff.Next()
+			c.log.Debugf("Retrying request in {%v} with timeout in {%v}", retryTime, c.requestTimeout)
+			time.Sleep(retryTime)
+
+			rpcResult, err = c.Request(c.serviceURL, c.serviceURL, requestID, cmdType, message)
+			if _, ok := err.(net.Error); ok {
+				continue
+			} else {
+				// We either succeeded or encountered a non connection error
+				break
+			}
+		}
+	}
+
+	return rpcResult, err
 }
 
 func (c *rpcClient) Request(logicalAddr *url.URL, physicalAddr *url.URL, requestID uint64,
 	cmdType pb.BaseCommand_Type, message proto.Message) (*RPCResult, error) {
 	c.metrics.RpcRequestCount.Inc()
-	cnx, err := c.getConn(logicalAddr, physicalAddr)
+	cnx, err := c.pool.GetConnection(logicalAddr, physicalAddr)
 	if err != nil {
 		return nil, err
 	}
@@ -109,27 +134,6 @@ func (c *rpcClient) Request(logicalAddr *url.URL, physicalAddr *url.URL, request
 	case <-time.After(c.requestTimeout):
 		return nil, errors.New("request timed out")
 	}
-}
-
-func (c *rpcClient) getConn(logicalAddr *url.URL, physicalAddr *url.URL) (Connection, error) {
-	cnx, err := c.pool.GetConnection(logicalAddr, physicalAddr)
-	backoff := Backoff{1 * time.Second}
-	startTime := time.Now()
-	var retryTime time.Duration
-	if err != nil {
-		for time.Since(startTime) < c.requestTimeout {
-			retryTime = backoff.Next()
-			c.log.Debugf("Reconnecting to broker in {%v} with timeout in {%v}", retryTime, c.requestTimeout)
-			time.Sleep(retryTime)
-			cnx, err = c.pool.GetConnection(logicalAddr, physicalAddr)
-			if err == nil {
-				c.log.Debugf("retry connection success")
-				return cnx, nil
-			}
-		}
-		return nil, err
-	}
-	return cnx, nil
 }
 
 func (c *rpcClient) RequestOnCnx(cnx Connection, requestID uint64, cmdType pb.BaseCommand_Type,