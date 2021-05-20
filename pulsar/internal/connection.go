275a94361285a031959214c2175e6cae696348e8
Matteo Merli <mmerli@apache.org>
2021-05-20 09:34:07 -0700

    Always check connection close channell, before attempting to put requests

--- pulsar/internal/connection.go
+++ pulsar/internal/connection.go
@@ -549,10 +549,15 @@ func (c *connection) SendRequest(requestID uint64, req *pb.BaseCommand,
 	if c.getState() == connectionClosed {
 		callback(req, ErrConnectionClosed)
 	} else {
-		c.incomingRequestsCh <- &request{
+		select {
+		case <-c.closeCh:
+			callback(req, ErrConnectionClosed)
+
+		case c.incomingRequestsCh <- &request{
 			id:       &requestID,
 			cmd:      req,
 			callback: callback,
+		}:
 		}
 	}
 }
@@ -562,12 +567,17 @@ func (c *connection) SendRequestNoWait(req *pb.BaseCommand) error {
 		return ErrConnectionClosed
 	}
 
-	c.incomingRequestsCh <- &request{
+	select {
+	case <-c.closeCh:
+		return ErrConnectionClosed
+
+	case c.incomingRequestsCh <- &request{
 		id:       nil,
 		cmd:      req,
 		callback: nil,
+	}:
+		return nil
 	}
-	return nil
 }
 
 func (c *connection) internalSendRequest(req *request) {
