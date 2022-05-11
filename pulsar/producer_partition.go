22c1d8eb960b63f236aecd6e0583f097680b88a6
xiaolongran <xiaolongran@tencent.com>
2022-05-11 22:49:43 +0800

    Fix  sequenceID is not equal to cause the connection to be closed incorrectly
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

--- pulsar/producer_partition.go
+++ pulsar/producer_partition.go
@@ -788,12 +788,12 @@ func (p *partitionProducer) ReceivedSendReceipt(response *pb.CommandSendReceipt)
 		// Ignoring the ack since it's referring to a message that has already timed out.
 		p.log.Warnf("Received ack for %v on sequenceId %v - expected: %v, closing connection", response.GetMessageId(),
 			response.GetSequenceId(), pi.sequenceID)
+		p._getConn().Close()
 		return
 	} else if pi.sequenceID > response.GetSequenceId() {
 		// Force connection closing so that messages can be re-transmitted in a new connection
 		p.log.Warnf("Received ack for %v on sequenceId %v - expected: %v, closing connection", response.GetMessageId(),
 			response.GetSequenceId(), pi.sequenceID)
-		p._getConn().Close()
 		return
 	} else {
 		// The ack was indeed for the expected item in the queue, we can remove it and trigger the callback
