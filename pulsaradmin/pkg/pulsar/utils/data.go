c382d67a3fc0e98e1716f02b81917a03afe490de
Will Nyffenegger <william.nyffenegger@iterable.com>
2022-10-18 22:38:50 -0500

    add estimated backlog size as measure of unconsumed messages (streamnative/pulsarctl#860)

--- pulsaradmin/pkg/pulsar/utils/data.go
+++ pulsaradmin/pkg/pulsar/utils/data.go
@@ -183,6 +183,7 @@ type NamespacesData struct {
 }
 
 type TopicStats struct {
+	BacklogSize         int64                        `json:"backlogSize"`
 	MsgCounterIn        int64                        `json:"msgInCounter"`
 	MsgCounterOut       int64                        `json:"msgOutCounter"`
 	MsgRateIn           float64                      `json:"msgRateIn"`
