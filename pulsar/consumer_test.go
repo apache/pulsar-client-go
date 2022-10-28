dca81593f9a3121353f0b6d077f3b7b16fb1c740
Jiaqi Shen <18863662628@163.com>
2022-10-28 15:14:13 +0800

    test: restart CI

--- pulsar/consumer_test.go
+++ pulsar/consumer_test.go
@@ -1481,7 +1481,6 @@ func TestRLQMultiTopics(t *testing.T) {
 		SubscriptionName:            subName,
 		SubscriptionInitialPosition: SubscriptionPositionEarliest,
 	})
-
 	assert.Nil(t, err)
 	defer dlqConsumer.Close()
 
