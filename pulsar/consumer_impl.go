3afd2d4a4fea7d24b0d7ef7b869a1eebeda8af2b
Jiaqi Shen <18863662628@163.com>
2022-12-21 20:50:31 +0800

    [Issue 918] [Refactor] Remove the clearMessageQueuesCh in partitionConsumer.dispatcher()

--- pulsar/consumer_impl.go
+++ pulsar/consumer_impl.go
@@ -589,7 +589,16 @@ func (c *consumer) Seek(msgID MessageID) error {
 		return err
 	}
 
-	return c.consumers[msgID.PartitionIdx()].Seek(msgID)
+	if err := c.consumers[msgID.PartitionIdx()].Seek(msgID); err != nil {
+		return err
+	}
+
+	// clear messageCh
+	for len(c.messageCh) > 0 {
+		<-c.messageCh
+	}
+
+	return nil
 }
 
 func (c *consumer) SeekByTime(time time.Time) error {
@@ -603,6 +612,12 @@ func (c *consumer) SeekByTime(time time.Time) error {
 			errs = pkgerrors.Wrap(newError(SeekFailed, err.Error()), msg)
 		}
 	}
+
+	// clear messageCh
+	for len(c.messageCh) > 0 {
+		<-c.messageCh
+	}
+
 	return errs
 }
 
