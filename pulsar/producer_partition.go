aa664716ac0bfb886c756f33ba72376092e8e3a5
gunli <24350715@qq.com>
2023-07-05 10:20:03 +0800

    [Fix] return when registerSendOrAckOp() failed (#1045)
    Master Issue: #1040
    
    ### Motivation
    
    
    fix #1040, return when registerSendOrAckOp() return an error.
    
    ### Modifications
    
    - modified: pulsar/producer_partition.go
    
    ---------
    
    Co-authored-by: gunli <gunli@tencent.com>

--- pulsar/producer_partition.go
+++ pulsar/producer_partition.go
@@ -1126,6 +1126,7 @@ func (p *partitionProducer) internalSendAsync(ctx context.Context, msg *Producer
 		}
 		if err := transactionImpl.registerSendOrAckOp(); err != nil {
 			runCallback(callback, nil, msg, err)
+			return
 		}
 		newCallback = func(id MessageID, producerMessage *ProducerMessage, err error) {
 			runCallback(callback, id, producerMessage, err)
