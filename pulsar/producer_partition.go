72b59c83f98bd1083f9c3704acf51d84c56919eb
gunli <gunli@tencent.com>
2023-10-29 11:42:12 +0800

    opt: update debug log

--- pulsar/producer_partition.go
+++ pulsar/producer_partition.go
@@ -1206,13 +1206,13 @@ func (p *partitionProducer) ReceivedSendReceipt(response *pb.CommandSendReceipt)
 
 	if pi.sequenceID < response.GetSequenceId() {
 		// Force connection closing so that messages can be re-transmitted in a new connection
-		p.log.Warnf("Received ack for %v on sequenceId %v - expected: %v, remote > local, closing connection",
+		p.log.Warnf("Received ack for %v on sequenceId %v - expected: %v, local < remote, closing connection",
 			response.GetMessageId(), response.GetSequenceId(), pi.sequenceID)
 		p._getConn().Close()
 		return
 	} else if pi.sequenceID > response.GetSequenceId() {
 		// Ignoring the ack since it's referring to a message that has already timed out.
-		p.log.Warnf("Received ack for %v on sequenceId %v - expected: %v, remote < local, ignore it",
+		p.log.Warnf("Received ack for %v on sequenceId %v - expected: %v, local > remote, ignore it",
 			response.GetMessageId(), response.GetSequenceId(), pi.sequenceID)
 		return
 	} else {
