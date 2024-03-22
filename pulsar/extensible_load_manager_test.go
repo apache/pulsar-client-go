1d237ccd6401026e26bffd18b4debe0a3b0dfdc7
Dragos Misca <dragosvictor@users.noreply.github.com>
2024-03-22 14:33:01 -0700

    Test refactor

--- pulsar/extensible_load_manager_test.go
+++ pulsar/extensible_load_manager_test.go
@@ -12,7 +12,9 @@ import (
 
 	"github.com/apache/pulsar-client-go/pulsaradmin"
 	"github.com/apache/pulsar-client-go/pulsaradmin/pkg/utils"
+
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
 	uAtomic "go.uber.org/atomic"
@@ -47,136 +49,46 @@ func (m *mockCounter) Inc() {
 	m.count.Inc()
 }
 
-func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnloadWithAssignedUrl() {
-	suite.T().Skipf("Skipping test until proxy issue is solved")
-	assertions := suite.Assert()
-
-	admin, err := pulsaradmin.NewClient(&pulsaradmin.Config{WebServiceURL: "http://broker-1:8080"})
-	assertions.Nil(err)
-
-	topicName, err := utils.GetTopicName(newTopicName())
-	assertions.Nil(err)
-	assertions.NotNil(topicName)
-
-	err = admin.Topics().Create(*topicName, 0)
-	assertions.Nil(err)
-
-	lookupResult, err := admin.Topics().Lookup(*topicName)
-	assertions.Nil(err)
-	assertions.NotEmpty(lookupResult.BrokerURL)
-	srcTopicBrokerURL := lookupResult.BrokerURL
-	assertions.Contains([...]string{broker1URL, broker2URL}, srcTopicBrokerURL)
-
-	var dstTopicBrokerURL string
-	if srcTopicBrokerURL == broker1URL {
-		dstTopicBrokerURL = broker2LookupURL
-	} else {
-		dstTopicBrokerURL = broker1LookupURL
+func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnload() {
+	type topicUnloadTestCase struct {
+		testCaseName       string
+		adminURL           string
+		clientEndpointFunc func(utils.LookupData) string
+		unloadEndpointFunc func(utils.LookupData) string
 	}
-
-	bundleRange, err := admin.Topics().GetBundleRange(*topicName)
-	assertions.Nil(err)
-	assertions.NotEmpty(bundleRange)
-
-	pulsarClient, err := NewClient(ClientOptions{URL: srcTopicBrokerURL})
-	assertions.Nil(err)
-	defer pulsarClient.Close()
-
-	producer, err := pulsarClient.CreateProducer(ProducerOptions{
-		Topic: topicName.String(),
-	})
-	assertions.Nil(err)
-	defer producer.Close()
-
-	consumer, err := pulsarClient.Subscribe(ConsumerOptions{
-		Topic:                       topicName.String(),
-		SubscriptionName:            fmt.Sprintf("my-sub-%v", time.Now().Nanosecond()),
-		SubscriptionInitialPosition: SubscriptionPositionEarliest,
-	})
-	assertions.Nil(err)
-	defer consumer.Close()
-
-	pulsarClientImpl := pulsarClient.(*client)
-	lookupRequestCounterMock := mockCounter{}
-	pulsarClientImpl.metrics.LookupRequestsCount = &lookupRequestCounterMock
-
-	messageCountBeforeUnload := 100
-	messageCountDuringUnload := 100
-	messageCountAfterUnload := 100
-	messageCount := messageCountBeforeUnload + messageCountDuringUnload + messageCountAfterUnload
-
-	// Signals all goroutines have completed
-	wgRoutines := sync.WaitGroup{}
-	wgRoutines.Add(2)
-
-	// Signals unload has completed
-	wgUnload := sync.WaitGroup{}
-	wgUnload.Add(1)
-
-	// Signals both producer and consumer have processed `messageCountBeforeUnload` messages
-	wgSendAndReceiveMessages := sync.WaitGroup{}
-	wgSendAndReceiveMessages.Add(2)
-
-	// Producer
-	go func() {
-		defer wgRoutines.Done()
-
-		for i := 0; i < messageCount; i++ {
-			if i == messageCountBeforeUnload+messageCountDuringUnload {
-				wgUnload.Wait()
-			}
-
-			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-			defer cancel()
-
-			pm := ProducerMessage{Payload: []byte(fmt.Sprintf("hello-%d", i))}
-			_, err := producer.Send(ctx, &pm)
-			assertions.Nil(err)
-			assertions.Nil(ctx.Err())
-
-			if i == messageCountBeforeUnload {
-				wgSendAndReceiveMessages.Done()
-			}
-		}
-	}()
-
-	// Consumer
-	go func() {
-		defer wgRoutines.Done()
-
-		for i := 0; i < messageCount; i++ {
-			if i == messageCountBeforeUnload+messageCountDuringUnload {
-				wgUnload.Wait()
-			}
-
-			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-			defer cancel()
-
-			_, err := consumer.Receive(ctx)
-			assertions.Nil(err)
-			assertions.Nil(ctx.Err())
-
-			if i == messageCountBeforeUnload {
-				wgSendAndReceiveMessages.Done()
-			}
-		}
-	}()
-
-	// Unload the bundle, triggering the producers and consumers to reconnect to the specified broker.
-	wgSendAndReceiveMessages.Wait()
-	unloadURL := fmt.Sprintf(
-		"/admin/v2/namespaces/%s/%s/%s/unload?destinationBroker=%s", tenant, namespace, bundleRange, dstTopicBrokerURL)
-	makeHTTPCall(suite.T(), http.MethodPut, lookupResult.HTTPURL+unloadURL, "")
-	wgUnload.Done()
-
-	wgRoutines.Wait()
-	assertions.Equal(int32(0), lookupRequestCounterMock.count.Load())
+	for _, scenario := range []topicUnloadTestCase{
+		{
+			testCaseName: "directConnection",
+			adminURL:     "http://broker-1:8080",
+			clientEndpointFunc: func(lookupResult utils.LookupData) string {
+				return lookupResult.BrokerURL
+			},
+			unloadEndpointFunc: func(lookupResult utils.LookupData) string {
+				return lookupResult.HTTPURL
+			},
+		},
+		{
+			testCaseName: "proxyConnection",
+			adminURL:     "http://proxy:8080",
+			clientEndpointFunc: func(utils.LookupData) string {
+				return "pulsar://proxy:6650"
+			},
+			unloadEndpointFunc: func(utils.LookupData) string {
+				return "http://proxy:8080"
+			},
+		},
+	} {
+		suite.T().Run(scenario.testCaseName, func(t *testing.T) {
+			testTopicUnload(t, scenario.adminURL, scenario.clientEndpointFunc, scenario.unloadEndpointFunc)
+		})
+	}
 }
 
-func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnloadWithAssignedUrlAndProxy() {
-	req := suite.Require()
+func testTopicUnload(t *testing.T, adminURL string,
+	clientEndpointFunc func(utils.LookupData) string,
+	unloadEndpointFunc func(utils.LookupData) string) {
+	req := assert.New(t)
 
-	adminURL := "http://proxy:8080"
 	admin, err := pulsaradmin.NewClient(&pulsaradmin.Config{WebServiceURL: adminURL})
 	req.NoError(err)
 
@@ -204,9 +116,8 @@ func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnloadWithAssignedUrlAndPr
 	req.NoError(err)
 	req.NotEmpty(bundleRange)
 
-	pulsarClient, err := NewClient(ClientOptions{
-		URL: "pulsar://proxy:6650",
-	})
+	clientURL := clientEndpointFunc(lookupResult)
+	pulsarClient, err := NewClient(ClientOptions{URL: clientURL})
 	req.NoError(err)
 	defer pulsarClient.Close()
 
@@ -259,8 +170,8 @@ func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnloadWithAssignedUrlAndPr
 
 			pm := ProducerMessage{Payload: []byte(fmt.Sprintf("hello-%d", i))}
 			_, err := producer.Send(ctx, &pm)
-			suite.NoError(err)
-			suite.NoError(ctx.Err())
+			req.NoError(err)
+			req.NoError(ctx.Err())
 
 			if i == messageCountBeforeUnload {
 				wgSendAndReceiveMessages.Done()
@@ -281,8 +192,8 @@ func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnloadWithAssignedUrlAndPr
 			defer cancel()
 
 			_, err := consumer.Receive(ctx)
-			suite.NoError(err)
-			suite.NoError(ctx.Err())
+			req.NoError(err)
+			req.NoError(ctx.Err())
 
 			if i == messageCountBeforeUnload {
 				wgSendAndReceiveMessages.Done()
@@ -292,9 +203,10 @@ func (suite *ExtensibleLoadManagerTestSuite) TestTopicUnloadWithAssignedUrlAndPr
 
 	// Unload the bundle, triggering the producers and consumers to reconnect to the specified broker.
 	wgSendAndReceiveMessages.Wait()
+	unloadEndpoint := unloadEndpointFunc(lookupResult)
 	unloadURL := fmt.Sprintf(
 		"/admin/v2/namespaces/%s/%s/%s/unload?destinationBroker=%s", tenant, namespace, bundleRange, dstTopicBrokerURL)
-	makeHTTPCall(suite.T(), http.MethodPut, adminURL+unloadURL, "")
+	makeHTTPCall(t, http.MethodPut, unloadEndpoint+unloadURL, "")
 	wgUnload.Done()
 
 	wgRoutines.Wait()
