205e41421773caf38a0d65fe3c42a2b631d58eeb
Ivan Penchev <ivan@penchev.com>
2024-07-04 09:39:08 +0200

    fix: opted to use any alias instead of interface - pr feedback

--- pulsar/log/wrapper_slog.go
+++ pulsar/log/wrapper_slog.go
@@ -26,39 +26,39 @@ type slogWrapper struct {
 	logger *slog.Logger
 }
 
-func (s *slogWrapper) Debug(args ...interface{}) {
+func (s *slogWrapper) Debug(args ...any) {
 	message := s.tryDetermineMessage(args)
 	s.logger.Debug(message)
 }
 
-func (s *slogWrapper) Info(args ...interface{}) {
+func (s *slogWrapper) Info(args ...any) {
 	message := s.tryDetermineMessage(args)
 	s.logger.Info(message)
 }
 
-func (s *slogWrapper) Error(args ...interface{}) {
+func (s *slogWrapper) Error(args ...any) {
 	message := s.tryDetermineMessage(args)
 	s.logger.Error(message)
 }
 
-func (s *slogWrapper) Warn(args ...interface{}) {
+func (s *slogWrapper) Warn(args ...any) {
 	message := s.tryDetermineMessage(args)
 	s.logger.Warn(message)
 }
 
-func (s *slogWrapper) Debugf(format string, args ...interface{}) {
+func (s *slogWrapper) Debugf(format string, args ...any) {
 	s.logger.Debug(fmt.Sprintf(format, args...))
 }
 
-func (s *slogWrapper) Infof(format string, args ...interface{}) {
+func (s *slogWrapper) Infof(format string, args ...any) {
 	s.logger.Info(fmt.Sprintf(format, args...))
 }
 
-func (s *slogWrapper) Warnf(format string, args ...interface{}) {
+func (s *slogWrapper) Warnf(format string, args ...any) {
 	s.logger.Warn(fmt.Sprintf(format, args...))
 }
 
-func (s *slogWrapper) Errorf(format string, args ...interface{}) {
+func (s *slogWrapper) Errorf(format string, args ...any) {
 	s.logger.Error(fmt.Sprintf(format, args...))
 }
 
@@ -72,7 +72,7 @@ func (s *slogWrapper) WithError(err error) Entry {
 	return s.WithField("error", err)
 }
 
-func (s *slogWrapper) WithField(name string, value interface{}) Entry {
+func (s *slogWrapper) WithField(name string, value any) Entry {
 	return &slogWrapper{
 		logger: s.logger.With(name, value),
 	}
@@ -90,15 +90,15 @@ func NewLoggerWithSlog(logger *slog.Logger) Logger {
 	}
 }
 
-func pulsarFieldsToKVSlice(f Fields) []interface{} {
-	ret := make([]interface{}, 0, len(f)*2)
+func pulsarFieldsToKVSlice(f Fields) []any {
+	ret := make([]any, 0, len(f)*2)
 	for k, v := range f {
 		ret = append(ret, k, v)
 	}
 	return ret
 }
 
-func (s *slogWrapper) tryDetermineMessage(value ...interface{}) string {
+func (s *slogWrapper) tryDetermineMessage(value ...any) string {
 	data, ok := value[0].(string)
 	if ok {
 		return data
