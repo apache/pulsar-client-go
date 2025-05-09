05c1e32c0abe23686c155d06f233272b317bd711
Masahiro Sakamoto <massakam@lycorp.co.jp>
2025-05-09 12:26:22 +0900

    Make ZTS proxy configurable in athenz auth plugin (#1360)

--- pulsar/auth/athenz.go
+++ pulsar/auth/athenz.go
@@ -50,6 +50,7 @@ type athenzAuthProvider struct {
 	principalHeader         string
 	roleHeader              string
 	ztsURL                  string
+	ztsProxyURL             string
 	tokenBuilder            zms.TokenBuilder
 	roleToken               zts.RoleToken
 	zmsNewTokenBuilder      func(domain, name string, privateKeyPEM []byte, keyVersion string) (zms.TokenBuilder, error)
@@ -78,6 +79,7 @@ func NewAuthenticationAthenzWithParams(params map[string]string) (Provider, erro
 		params["principalHeader"],
 		params["roleHeader"],
 		params["ztsUrl"],
+		params["ztsProxyUrl"],
 	), nil
 }
 
@@ -91,7 +93,8 @@ func NewAuthenticationAthenz(
 	caCert string,
 	principalHeader string,
 	roleHeader string,
-	ztsURL string) Provider {
+	ztsURL string,
+	ztsProxyURL string) Provider {
 	fixedKeyID := defaultKeyID
 	if keyID != "" {
 		fixedKeyID = keyID
@@ -121,6 +124,7 @@ func NewAuthenticationAthenz(
 		principalHeader:         principalHeader,
 		roleHeader:              fixedRoleHeader,
 		ztsURL:                  strings.TrimSuffix(ztsURL, "/"),
+		ztsProxyURL:             ztsProxyURL,
 		zmsNewTokenBuilder:      zms.NewTokenBuilder,
 		ztsNewRoleToken:         ztsNewRoleToken,
 		ztsNewRoleTokenFromCert: ztsNewRoleTokenFromCert,
@@ -135,6 +139,7 @@ func (p *athenzAuthProvider) Init() error {
 	var roleToken zts.RoleToken
 	opts := zts.RoleTokenOptions{
 		BaseZTSURL:       p.ztsURL + "/zts/v1",
+		ProxyURL:         p.ztsProxyURL,
 		MinExpire:        minExpire,
 		MaxExpire:        maxExpire,
 		PrefetchInterval: prefetchInterval,
