597fb3c912b037fd349507f46adebc1603d064e0
Zike Yang <zike@apache.org>
2025-05-20 15:29:25 +0800

    Fix the staging release process

--- docs/release-process.md
+++ docs/release-process.md
@@ -51,19 +51,10 @@ git push origin v0.X.0-candidate-1
 
 3. Build and inspect an artifact.
 
-Generate a release candidate package.
+Stage and sigh the release artifacts.
 
 ```bash
-$ tar -zcvf apache-pulsar-client-go-0.X.0-src.tar.gz .
-```
-
-4. Sign and stage the artifacts 
-
-The src artifact need to be signed and uploaded to the dist SVN repository for staging.
-
-```
-$ gpg -b --armor apache-pulsar-client-go-0.X.0-src.tar.gz
-$ shasum -a 512 apache-pulsar-client-go-0.X.0-src.tar.gz > apache-pulsar-client-go-0.X.0-src.tar.gz.sha512 
+$ VERSION=0.x.0 ./scripts/stage-release.sh .
 ```
 
 Checkout repo for uploading artifacts
---
+++ scripts/stage-release.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+set -e -x
+
+if [ $# -eq 0 ]; then
+    echo "Required argument with destination directory"
+    exit 1
+fi
+
+if [ -z "$VERSION" ]; then
+    echo "VERSION is not defined. Please set the VERSION environment variable."
+    exit 1
+fi
+
+
+DEST_PATH=$1
+DEST_PATH="$(cd "$DEST_PATH" && pwd)"
+
+pushd $(dirname "$0")
+REPO_PATH=$(git rev-parse --show-toplevel)
+popd
+
+pushd $REPO_PATH
+git archive --format=tar.gz --output="$DEST_PATH/apache-pulsar-client-go-$VERSION-src.tar.gz" HEAD
+popd
+
+# Sign all files
+cd $DEST_PATH
+gpg -b --armor apache-pulsar-client-go-$VERSION-src.tar.gz
+shasum -a 512 apache-pulsar-client-go-$VERSION-src.tar.gz > apache-pulsar-client-go-$VERSION-src.tar.gz.sha512
+
