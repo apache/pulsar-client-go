bdfa6a08d6b22ef367bda73b133424c45d710e8f
Cong Zhao <zhaocong@apache.org>
2025-11-13 21:59:49 +0800

    Fix the issue of unable to parse non-batch messages that with non-empty properties and empty payloads. (#1435)
    ### Motivation
    
    ```
    time="2025-11-03T12:22:21+08:00" level=info msg="Created producer" cnx="[::1]:55372 -> [::1]:6650" producerID=1 producer_name=standalone-0-14 topic="persistent://public/default/my-topic-965686000"
    time="2025-11-03T12:22:21+08:00" level=error msg="Discarding corrupted message" consumerID=1 msgID="ledgerId:19 entryId:0 partition:-1" name= subscription=reader-ebjpf topic="persistent://public/default/my-topic-965686000" validationError=BatchDeSerializeError
    ```
    
    ### Modifications
    
    Correct parse non-batch messages that with non-empty properties and empty payloads.

--- pulsar/internal/commands_test.go
+++ pulsar/internal/commands_test.go
@@ -98,8 +98,9 @@ func TestReadMessageOldFormat(t *testing.T) {
 	assert.Equal(t, true, ssm == nil)
 	assert.Equal(t, "hello", string(payload))
 
-	_, _, err = reader.ReadMessage()
-	assert.Equal(t, ErrEOM, err)
+	_, payload, err = reader.ReadMessage()
+	assert.Equal(t, []byte{}, payload)
+	assert.Nil(t, err)
 }
 
 func TestReadMessagesBatchSize1(t *testing.T) {
