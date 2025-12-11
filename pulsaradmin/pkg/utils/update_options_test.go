7621205e21c3ac6a4d676b5617deef474dafc633
Rui Fu <rfu@apache.org>
2025-12-11 22:45:49 +0800

    add tests

---
+++ pulsaradmin/pkg/utils/update_options_test.go
@@ -0,0 +1,45 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package utils
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdateOptionsMarshalJSON(t *testing.T) {
+	opts := UpdateOptions{UpdateAuthData: true}
+
+	data, err := json.Marshal(opts)
+	require.NoError(t, err)
+
+	assert.JSONEq(t, `{"updateAuthData":true}`, string(data))
+}
+
+func TestUpdateOptionsUnmarshalJSON(t *testing.T) {
+	payload := `{"updateAuthData":true}`
+
+	var opts UpdateOptions
+	err := json.Unmarshal([]byte(payload), &opts)
+	require.NoError(t, err)
+
+	assert.True(t, opts.UpdateAuthData)
+}
